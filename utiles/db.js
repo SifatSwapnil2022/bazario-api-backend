8e9b6cbffc9f2dfe78cd971732074d6607d78e13
SifatSwapnil2022 <mdsifatullahsheikh@gmail.com>
2025-08-10 18:12:04 +0600

    db_url_issues

--- utiles/db.js
+++ utiles/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 module.exports.dbConnect = async () => {
-    const mode = process.env.mode
+    
     try {
-        if (mode === 'production') {
-            await mongoose.connect(process.env.DB_PRODUCTION_URL, { useNewURLParser: true })
+        if (process.env.mode === 'pro') {
+            await mongoose.connect(process.env.DB_PRO_URL, { useNewURLParser: true })
             console.log("Production database connect....")
         } else {
             await mongoose.connect(process.env.DB_LOCAL_URL, { useNewURLParser: true })
